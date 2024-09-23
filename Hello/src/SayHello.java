a8ce4256db63f86dd8dfc33f51cf4aa0084e3c5b
ThuanPC <thuannhuynnh@gmail.com>
2024-09-23 11:38:59 +0700

    first commit - done

---
+++ Hello/src/SayHello.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class SayHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World! From NTU ThuanPC");
+	}
+
+}
