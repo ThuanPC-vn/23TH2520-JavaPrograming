5ef6d30b2639058683b3bcb40ff89978d1991605
ThuanPC <thuannhuynnh@gmail.com>
2024-09-27 11:31:24 +0700

    update - create project Lab1.3 - done

---
+++ Lab1.3/src/Lab1Bai3.java
@@ -0,0 +1,26 @@
+/**
+ * Declare library
+ */
+import java.util.Scanner;
+
+
+
+public class Lab1Bai3 {
+	
+	/**
+	* Hàm tính thể tích của khối lập phương
+	* @access public
+	* @param double
+	* @return double
+	*/
+    public static double calculateVolume(double edge) {
+        return Math.pow(edge, 3);
+    }
+    
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
