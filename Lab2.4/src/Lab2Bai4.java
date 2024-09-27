136c530d1c8d4136076bba5f922e70153dc2c646
ThuanPC <thuannhuynnh@gmail.com>
2024-09-27 16:25:15 +0700

    update - complete function giai PT Bac2  - done

--- Lab2.4/src/Lab2Bai4.java
+++ Lab2.4/src/Lab2Bai4.java
@@ -67,6 +67,38 @@ public class Lab2Bai4 {
 	
 	
 	
+	/**
+	* Hàm tính PT bậc hai
+	* @access public
+	* @param null
+	* @return null
+	*/
+	public static void giaiPTB2() {
+        // Mã giải phương trình bậc 2
+        System.out.println("Giải phương trình bậc hai ax^2 + bx + c = 0");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập a: ");
+        double a = scanner.nextDouble();
+        System.out.print("Nhập b: ");
+        double b = scanner.nextDouble();
+        System.out.print("Nhập c: ");
+        double c = scanner.nextDouble();
+        double delta = b * b - 4 * a * c;
+        if (delta > 0) {
+            double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+            double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+            System.out.println("Phương trình có hai nghiệm phân biệt: x1 = " + x1 + ", x2 = " + x2);
+        } else if (delta == 0) {
+            double x = -b / (2 * a);
+            System.out.println("Phương trình có nghiệm kép: x = " + x);
+        } else {
+            System.out.println("Phương trình vô nghiệm.");
+        }
+    }
+	
+	
+	
+	
 	
 	
 	
