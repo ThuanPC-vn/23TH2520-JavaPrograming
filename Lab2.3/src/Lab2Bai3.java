635699fc174cba3f711218888d7e7980f99b78b0
ThuanPC <thuannhuynnh@gmail.com>
2024-09-27 16:12:10 +0700

    update - Complete the Lab2 Bai3 - done

--- Lab2.3/src/Lab2Bai3.java
+++ Lab2.3/src/Lab2Bai3.java
@@ -25,7 +25,21 @@ public class Lab2Bai3 {
         return tien;
     }
 	
+	
+	
+	/**
+	* Hàm main gọi đến các function trên
+	* @access public
+	* @param args
+	* @return null
+	*/
 	public static void main(String[] args) {
-		
+		Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số điện sử dụng trong tháng: ");
+        int soDien = scanner.nextInt();
+        
+        // Gọi hàm tính Tiền Điện
+        int tien = tinhTienDien(soDien);
+        System.out.println("Số tiền điện phải trả là: " + tien + " VNĐ");
 	}
 }
