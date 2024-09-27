c6f50fca045e8068a377d82cc1153e518d393f42
ThuanPC <thuannhuynnh@gmail.com>
2024-09-27 16:55:36 +0700

    update - create project chDoiDonVi - done

---
+++ chDoiDonVi/src/chDoiDonVi/DoiDonVi.java
@@ -0,0 +1,38 @@
+package chDoiDonVi;
+
+public class DoiDonVi {
+	
+	/**
+	* Hàm đổi km sang m
+	* @access public
+	* @param double
+	* @return double
+	*/
+    public static double kmToM(double km) {
+        return km * 1000;
+    }
+
+    /**
+	* Hàm đổi byte sang bit
+	* @access public
+	* @param double
+	* @return double
+	*/
+    public static double byteToBit(double bytes) {
+        return bytes * 8;
+    }
+    
+    
+    
+    /**
+	* Hàm main gọi đến các function trên và hiển trị kết quả
+	* @access public
+	* @param args
+	* @return null
+	*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
