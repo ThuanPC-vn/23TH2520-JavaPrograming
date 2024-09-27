9fb3ea5fcd7090a7c4047c1e8bf453cb72badbff
ThuanPC <thuannhuynnh@gmail.com>
2024-09-27 16:23:45 +0700

    update - complete function menu  - done

--- Lab2.4/src/Lab2Bai4.java
+++ Lab2.4/src/Lab2Bai4.java
@@ -1,9 +1,48 @@
+/**
+* Declare library
+*/
+import java.util.Scanner;
+
+
 
 public class Lab2Bai4 {
-
+	
+	/**
+	* Hàm main gọi đến các function dưới
+	* @access public
+	* @param args
+	* @return null
+	*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
 
+	
+	
+	/**
+	* Hàm hiện thị menu
+	* @access public
+	* @param null
+	* @return null
+	*/
+	public static void menu() {
+        System.out.println("+---------------------------------------------------+");
+        System.out.println("|   1. Giải phương trình bậc nhất     |");
+        System.out.println("|   2. Giải phương trình bậc 2          |");
+        System.out.println("|   3. Tính tiền điện                           |");
+        System.out.println("|   4. Kết thúc                                    |");
+        System.out.println("+---------------------------------------------------+");
+        System.out.print("Chọn chức năng: ");
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
